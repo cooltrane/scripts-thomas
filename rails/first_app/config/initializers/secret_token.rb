f5b71b5fc982b551d42a38a0cd7396ab5a5905f7
cooltrane <thomas@plugin-services.com>
2012-02-26 17:14:48 +0100

    suppression de l'appli de test ruby

--- rails/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '5dba6a0f7e60a041ebf099f16d61e7b8cac16ace22d003b95673320aac9adc56e765982c36e1176953882146f6b2781f41d89ccade5f3918cdbf64159f1e7df3'
